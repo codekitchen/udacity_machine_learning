ad1b378d9379ae043aef1a0b15e4dd61e874ca80
Brian Palmer <brian@codekitchen.net>
2017-08-08 15:50:55 -0600

    install pygame and make it work with dinghy

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM gw000/keras-full
 
 RUN pip install --upgrade 'scikit-learn<0.18' \
- && pip install 'seaborn'
+ && pip install 'seaborn' 'pygame'
--- run-keras.sh
+++ run-keras.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+open -a XQuartz
+xhost + $(dinghy ip)
 echo "open http://keras.docker/"
-docker run -it --rm -e VIRTUAL_HOST=keras.docker:8888 -v=$(pwd):/srv keras
+docker run -it --rm -e VIRTUAL_HOST=keras.docker:8888 -e DISPLAY=$(dinghy ip --host):0 -v=$(pwd):/srv keras
